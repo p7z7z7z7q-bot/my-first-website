44ee252307f4dfaddfbe5b65450abb9382a6269f
rm-studio-dev <p7z7z7z7q@gmail.com>
2025-12-25 19:21:55 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// فقط دسکتاپ، زیرمنو با hover باز میشه، نیازی به موبایل نیست
+// دسکتاپ فقط، زیرمنو با hover کار میکنه
 const dropdowns = document.querySelectorAll('.dropbtn');
 
 dropdowns.forEach(btn => {
     btn.addEventListener('click', (e) => {
-        e.preventDefault(); // جلوگیری از رفتن لینک
+        e.preventDefault();
         const content = btn.nextElementSibling;
         content.classList.toggle('open');
     });
