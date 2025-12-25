145d01e5fa80429175074ad19f9fc2868692f587
rm-studio-dev <p7z7z7z7q@gmail.com>
2025-12-25 19:38:14 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// دسکتاپ فقط، زیرمنو با hover کار میکنه
-const dropdowns = document.querySelectorAll('.dropbtn');
 
-dropdowns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        e.preventDefault();
-        const content = btn.nextElementSibling;
-        content.classList.toggle('open');
-    });
-});
